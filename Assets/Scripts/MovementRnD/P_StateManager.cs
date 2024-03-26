eeb06b2581d6287b50e86afff5621e33c85df6c4
LoveO98 <olsson.love98@gmail.com>
2024-03-26 19:29:00 +0100

    Added the most basic jump
    Also prepping for merge with main

--- Assets/Scripts/MovementRnD/P_StateManager.cs
+++ Assets/Scripts/MovementRnD/P_StateManager.cs
@@ -5,28 +5,29 @@ using UnityEngine.InputSystem;
 
 public class P_StateManager : MonoBehaviour
 {
+    //I'm using "_" for every variable that's declared in the class and not using it for the ones declared in methods. Should make it easier to see which one belongs where at a glance. Please follow this convention to the best of your abilities.
+    PlayerInput _playerInput;
+    CharacterController _characterController;
+    Animator _animator;
 
-    PlayerInput playerInput;
-    CharacterController characterController;
-    Animator animator;
-
-    int isWalkingHash;
-    int isSprintingHash;
-    int isFallingHash;
+    int _isWalkingHash;
+    int _isSprintingHash;
+    int _isFallingHash;
 
     P_BaseState _currentState;
     P_StateFactory _states;
 
     //Variables to store player inputs
-    Vector2 currentMovementInput;
-    Vector3 currentMovement;
-    Vector3 currentSprintMovement;
-    bool isMovementPressed;
-    bool isSprintPressed;
-    
+    Vector2 _currentMovementInput;
+    Vector3 _currentMovement;
+    Vector3 _currentSprintMovement;
+    bool _isMovementPressed;
+    bool _isSprintPressed;
 
-    float rotationFactorPerFrame = 10f;
-    float sprintMultiplier = 3f;
+    bool _isJumpPressed = false;
+
+    float _rotationFactorPerFrame = 10f;
+    float _sprintMultiplier = 3f;
 
     //Put a lot of getters and setters here
     public P_BaseState CurrentState { get { return _currentState; } set { _currentState = value; } }
@@ -40,19 +41,21 @@ public class P_StateManager : MonoBehaviour
 
     private void Awake()
     {
-        playerInput = new PlayerInput();
-        characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
+        _playerInput = new PlayerInput();
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
 
-        isWalkingHash = Animator.StringToHash("isWalking");
-        isSprintingHash = Animator.StringToHash("isRunning");
-        isFallingHash = Animator.StringToHash("isFalling");
+        _isWalkingHash = Animator.StringToHash("isWalking");
+        _isSprintingHash = Animator.StringToHash("isRunning");
+        _isFallingHash = Animator.StringToHash("isFalling");
 
-        playerInput.CharacterControls.Move.started += onMovementInput;
-        playerInput.CharacterControls.Move.canceled += onMovementInput;
-        playerInput.CharacterControls.Move.performed += onMovementInput; //This allows the game to realize we might be holding two buttons at once (based). It also allows for controler inputs (cringe)
-        playerInput.CharacterControls.Sprint.started += onSprint;
-        playerInput.CharacterControls.Sprint.canceled += onSprint;
+        _playerInput.CharacterControls.Move.started += OnMovementInput;
+        _playerInput.CharacterControls.Move.canceled += OnMovementInput;
+        _playerInput.CharacterControls.Move.performed += OnMovementInput; //This allows the game to realize we might be holding two buttons at once (based). It also allows for controler inputs (cringe)
+        _playerInput.CharacterControls.Sprint.started += OnSprint;
+        _playerInput.CharacterControls.Sprint.canceled += OnSprint;
+        _playerInput.CharacterControls.Jump.started += OnJump;
+        _playerInput.CharacterControls.Jump.canceled += OnJump;
         
 
         //setup state
@@ -63,127 +66,138 @@ public class P_StateManager : MonoBehaviour
 
     }
 
-    void handleRotation()
+    void HandleRotation()
     {
         Vector3 positionToLookAt;
 
-        positionToLookAt.x = currentMovement.x;
+        positionToLookAt.x = _currentMovement.x;
         positionToLookAt.y = 0.0f;
-        positionToLookAt.z = currentMovement.z;
+        positionToLookAt.z = _currentMovement.z;
 
         Quaternion currentRotation = transform.rotation;
 
-        if (isMovementPressed)
+        if (_isMovementPressed)
         {
             Quaternion targetRotation = Quaternion.LookRotation(positionToLookAt);
-            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, _rotationFactorPerFrame * Time.deltaTime);
         }
         
     }
 
-    
-
-    void onSprint(InputAction.CallbackContext context)
+    void OnJump(InputAction.CallbackContext context)
     {
-        isSprintPressed = context.ReadValueAsButton();
+        _isJumpPressed = context.ReadValueAsButton();
     }
 
-    void onMovementInput(InputAction.CallbackContext context)
+    void OnSprint(InputAction.CallbackContext context)
     {
-        currentMovementInput = context.ReadValue<Vector2>();
-        currentMovement.x = currentMovementInput.x;
-        currentMovement.z = currentMovementInput.y;
-        currentSprintMovement.x = currentMovementInput.x * sprintMultiplier;
-        currentSprintMovement.z = currentMovementInput.y * sprintMultiplier;
-        isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+        _isSprintPressed = context.ReadValueAsButton();
     }
 
-    void handleAnimation()
+    void OnMovementInput(InputAction.CallbackContext context)
     {
-        bool isWalking = animator.GetBool(isWalkingHash);
-        bool isSprinting = animator.GetBool(isSprintingHash);
-        bool isFalling = animator.GetBool(isFallingHash);
+        _currentMovementInput = context.ReadValue<Vector2>();
+        _currentMovement.x = _currentMovementInput.x;
+        _currentMovement.z = _currentMovementInput.y;
+        _currentSprintMovement.x = _currentMovementInput.x * _sprintMultiplier;
+        _currentSprintMovement.z = _currentMovementInput.y * _sprintMultiplier;
+        _isMovementPressed = _currentMovementInput.x != 0 || _currentMovementInput.y != 0;
+    }
 
-        if (!characterController.isGrounded && !isFalling)
+    void HandleAnimation()
+    {
+        bool isWalking = _animator.GetBool(_isWalkingHash);
+        bool isSprinting = _animator.GetBool(_isSprintingHash);
+        bool isFalling = _animator.GetBool(_isFallingHash);
+
+        if (!_characterController.isGrounded && !isFalling)
         {
-            animator.SetBool(isFallingHash, true);
+            _animator.SetBool(_isFallingHash, true);
         }
-        else if(characterController.isGrounded && isFalling)
+        else if(_characterController.isGrounded && isFalling)
         {
-            animator.SetBool(isFallingHash, false);
+            _animator.SetBool(_isFallingHash, false);
         }
 
-        if(isMovementPressed && !isWalking)
+        if(_isMovementPressed && !isWalking)
         {
-            animator.SetBool(isWalkingHash, true);
+            _animator.SetBool(_isWalkingHash, true);
         }
-        else if(!isMovementPressed && isWalking)
+        else if(!_isMovementPressed && isWalking)
         {
-            animator.SetBool(isWalkingHash, false);
+            _animator.SetBool(_isWalkingHash, false);
         }
 
-        if((isMovementPressed && isSprintPressed) && !isSprinting)
+        if(_isMovementPressed && _isSprintPressed && !isSprinting)
         {
-            animator.SetBool(isSprintingHash, true);
+            _animator.SetBool(_isSprintingHash, true);
         }
-        else if((!isMovementPressed || !isSprintPressed) && isSprinting)
+        else if((!_isMovementPressed || !_isSprintPressed) && isSprinting)
         {
-            animator.SetBool(isSprintingHash, false);
+            _animator.SetBool(_isSprintingHash, false);
 
         }
 
     }
 
-    void handleGravity()
+    void HandleGravity()
     {
-        if (characterController.isGrounded)
+        if (_characterController.isGrounded)
         {
-            float groundedGravity = -.05f;
-            currentMovement.y = groundedGravity;
-            currentSprintMovement.y = groundedGravity;
+            float groundedGravity = -.05f;      //isGrounded only returns true if the character is touching the ground and has a downwards momentum
+            _currentMovement.y = groundedGravity;
+            _currentSprintMovement.y = groundedGravity;
         }
         else
         {
             float gravity = -9.8f;
-            currentMovement.y += gravity * Time.deltaTime;
-            currentSprintMovement.y += gravity * Time.deltaTime;
+            _currentMovement.y += gravity * Time.deltaTime;
+            _currentSprintMovement.y += gravity * Time.deltaTime;
         }
 
         
 
     }
 
+    void HandleJump()
+    {
+        if(_isJumpPressed && _characterController.isGrounded)
+        {
+            _currentMovement.y = 4;
+        }
+    }
+
     void Update()
     {
-        handleRotation();
-        handleAnimation();
-        handleGravity();
-        
+        HandleRotation();
+        HandleAnimation();
+        HandleJump();
 
-        if (isSprintPressed)
+        if (_isSprintPressed)
         {
-            characterController.Move(currentSprintMovement * Time.deltaTime);
+            _characterController.Move(_currentSprintMovement * Time.deltaTime);
 
         }
         else
         {
-            characterController.Move(currentMovement * Time.deltaTime);
+            _characterController.Move(_currentMovement * Time.deltaTime);
         }
 
         
 
+        HandleGravity();
         _currentState.UpdateStates();
     }
 
 
     void OnEnable()
     {
-        playerInput.CharacterControls.Enable();
+        _playerInput.CharacterControls.Enable();
     }
 
     void OnDisable()
     {
-        playerInput.CharacterControls.Disable();
+        _playerInput.CharacterControls.Disable();
     }
 
 
