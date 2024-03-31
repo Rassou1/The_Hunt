a1723e91f7657aa4c0ac39f379f53783044685c0
LoveO98 <olsson.love98@gmail.com>
2024-03-31 15:48:14 +0200

    State machine rigidbody
    The state machine now works with a rigidbody. It's really barebones though and we lost a lot of basic functionality. Will need a lot of more work to get working good. Animations also broke, but they aren't really a prio rn

--- Assets/Scripts/PlayerStateMachine/Prey/P_InAirState.cs
+++ Assets/Scripts/PlayerStateMachine/Prey/P_InAirState.cs
@@ -31,7 +31,7 @@ public class P_InAirState : P_BaseState
 
     public override void CheckSwitchState()
     {
-        if (_ctx.CharacterController.isGrounded)
+        if (_ctx.IsGrounded)
         {
             SwitchState(_factory.Ground());
         }
@@ -59,7 +59,7 @@ public class P_InAirState : P_BaseState
     {
         bool goingDown = _ctx.CurrentMovementY <= 2.0f || !_ctx.IsJumpPressed;
         float fallMultiplier = 2f;
-        
+
         if (goingDown)
         {
             float previousYVelocity = _ctx.CurrentMovementY;
