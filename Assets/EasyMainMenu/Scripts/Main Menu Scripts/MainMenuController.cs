06f50a1e4fe12313dfd3b2827328d1dda70af937
tyronnr1 <tyronsantospereira@gmail.com>
2024-04-03 22:45:29 +0200

    bassicaly just added mult to endproduct

--- Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
