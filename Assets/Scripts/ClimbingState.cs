fc125967c395a11d30cfb15b53b070e494114da6
LaraBerg <handeggul@gmail.com>
2024-04-09 15:45:54 +0200

    ...
    climbing not works for now

--- Assets/Scripts/ClimbingState.cs
+++ Assets/Scripts/ClimbingState.cs
@@ -35,19 +35,19 @@ public class P_ClimbingState : P_BaseState
 
     public override void CheckSwitchState()
     {
-        // Ladder layer?ndaki objelerle temas? kontrol et
+        
         Collider[] colliders = Physics.OverlapSphere(_ctx.transform.position, 0.5f, LayerMask.GetMask("Ladder"));
 
         if (colliders.Length == 0)
         {
-            // E?er merdivene temas kesildiyse veya t?rmanma tu?u b?rak?ld?ysa, Idle durumuna geç
+           
             SwitchState(_factory.Idle());
         }
 
-        // T?rmanma i?lemi s?ras?nda t?rmanma tu?u b?rak?ld?ysa veya t?rmanma i?lemi sonland?r?ld?ysa
+       
         if (!_ctx.IsClimbingPressed || !_isClimbing)
         {
-            // T?rmanma durumunu s?f?rla ve yere in
+            
             SwitchState(_factory.Ground());
         }
     }
