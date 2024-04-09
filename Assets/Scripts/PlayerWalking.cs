f4a4b8298e0bddb1376789560124241de15d9fa0
Jonathanll3 <jonathan.lundin.lundquist@gmail.com>
2024-04-09 13:09:55 +0200

    What is love?

--- Assets/Scripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerWalking.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerWalking : MonoBehaviour
 {
+    AudioSource AudioManager;
+    public AudioClip[] Footsteps;
+    Ray floorRay;
+    RaycastHit hit;
+    string floorName = string.Empty;
     // Start is called before the first frame update
     void Start()
     {
+        AudioManager = GetComponent<AudioSource>();
         
+        Footsteps = Resources.LoadAll<AudioClip>("Sounds/Footsteps/Footsteps_Grass/Walk");
+        floorRay = new Ray(transform.position, transform.up * -1);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()   
     {
-        
+        if (Physics.Raycast(floorRay,out hit,100))
+        {
+            floorName = hit.transform.gameObject.tag;
+            floorName = floorName.Replace("ground", "");
+            Footsteps = Resources.LoadAll<AudioClip>($"Sounds/Footsteps/Footsteps_{floorName}/Walk");
+        }
+    }
+
+    public void PlayWalkSound()
+    {
+        int footstep = Random.Range(0, Footsteps.Length);
+        Debug.Log($"Played Walking sound number {footstep}");
+        if (!AudioManager.isPlaying)
+        {
+            AudioManager.PlayOneShot(Footsteps[footstep]);
+        }
+    }
+
+    public void PlayJumpSound()
+    {
+
+    }
+
+    public void PlayRunSound()
+    {
+
     }
 }
