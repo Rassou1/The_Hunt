099ba824e14e8f3022328106afe39247ea21fa0e
LaraBerg <handeggul@gmail.com>
2024-04-27 11:46:28 +0200

    ...

--- Assets/LaDoor/WinTrigger.cs
+++ Assets/LaDoor/WinTrigger.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
+
     public GameObject youWinText;
+    public float delay;
     void Start()
     {
       youWinText.SetActive(false);  
@@ -17,6 +20,15 @@ public class WinTrigger : MonoBehaviour
         if (other.gameObject.tag == "Player") 
         {
             youWinText.SetActive(true);
+            StartCoroutine(CountDown());
         }
+    }
+
+    IEnumerator CountDown ()
+    {
+        yield return new WaitForSeconds(delay);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(0);
     }
 }
