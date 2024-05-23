1b5597e7397f781bad270a87ac412cf1923c0b64
tyronnr1 <tyronsantospereira@gmail.com>
2024-05-23 17:13:37 +0200

    armrender

--- Assets/Scripts/PlayerStateMachine/Prey/P_MoveCamera.cs
+++ Assets/Scripts/PlayerStateMachine/Prey/P_MoveCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Alteruna;
+using Unity.VisualScripting;
 public class P_MoveCamera : MonoBehaviour
 {
     public Transform cameraPosition;
@@ -29,7 +30,48 @@ public class P_MoveCamera : MonoBehaviour
         else
         {
             parentRenderer.enabled = true;
+
+            List<GameObject> _players = FindObjectsOnLayer(9);
+
+            foreach (var obj in _players)
+            {
+
+                Transform parentTransform = obj.transform;
+
+                // Find the child GameObjects by name
+                Transform firstChild = parentTransform.Find("PreyComponent");
+                Transform orientation = firstChild.Find("Orientation");
+                Transform camerahold = orientation.Find("CameraHolder");
+                Transform cam = camerahold.Find("PlayerCam"); 
+                Transform arm = cam.Find("PreyFPSArms");
+                Transform rend = arm.Find("PreyV2Rabbit_3JointRig.009");
+
+
+                rend.gameObject.GetComponent<Renderer>().enabled = false;
+
+                //Transform secondChild = parentTransform.Find("HunterComponent");
+                //secondChild.gameObject.GetComponent<Renderer>().enabled = false;
+
+            }
         }
+
+
+        
     }
 
+    List<GameObject> FindObjectsOnLayer(int layer)
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        List<GameObject> objectsInLayer = new List<GameObject>();
+
+        foreach (var obj in allPlayers)
+        {
+            if (obj.layer == layer)
+            {
+                objectsInLayer.Add(obj);
+            }
+        }
+
+        return objectsInLayer;
+    }
 }
