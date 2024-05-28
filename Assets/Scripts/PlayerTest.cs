ee3c9d27033a6819be4844f2b18a0dd0746565c8
Jonathanll3 <jonathan.lundin.lundquist@gmail.com>
2024-05-28 12:11:34 +0200

    Fixed so you can leavy and join

--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -30,17 +30,17 @@ public class PlayerTest : AttributesSync
         //Debug.LogError(_avatar.transform.position);
         _audioSoruce.spatialBlend = 1.0f;
 
-        if (new Vector3(_avatar.transform.position.x, 0, _avatar.transform.position.z) != new Vector3(oldPosition.x, 0, oldPosition.z))
-        {
-            _playerSounds.PlayWalkSound();
-            //if (false)
-            //{
-            //}
-            //else
-            //{
-            //    _playerSounds.PlayRunSound();
-            //}
-        }
+        //if (new Vector3(_avatar.transform.position.x, 0, _avatar.transform.position.z) != new Vector3(oldPosition.x, 0, oldPosition.z))
+        //{
+        //    _playerSounds.PlayWalkSound();
+        //    //if (false)
+        //    //{
+        //    //}
+        //    //else
+        //    //{
+        //    //    _playerSounds.PlayRunSound();
+        //    //}
+        //}
 
         if (Vector3.Distance(new Vector3(_avatar.transform.position.x, 0, _avatar.transform.position.z), new Vector3(oldPosition.x, 0, oldPosition.z)) > 3)
         {
--- Assets/Scripts/Voice/VivoxTest.cs
+++ Assets/Scripts/Voice/VivoxTest.cs
@@ -11,26 +11,27 @@ using UnityEngine.UI;
 public class VivoxTest : MonoBehaviour
 {
     Multiplayer multiplayer;
-    // Start is called before the first frame update
+    string currentVoicechatLobby;
     async void Start()
     {
         multiplayer = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Multiplayer>();
-        await UnityServices.InitializeAsync();
-        await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        await VivoxService.Instance.InitializeAsync();
-        VivoxService.Instance.LoggedIn += UserLoggedIn;
-        multiplayer.OnRoomJoined.AddListener(LogInToVivox);
-        //multiplayer.OnRoomLeft.AddListener(LeftChannel);
-
-        //await VivoxService.Instance.JoinEchoChannelAsync("EchoTest", ChatCapability.TextAndAudio);
-        //await VivoxService.Instance.JoinGroupChannelAsync("ChannelName", ChatCapability.AudioOnly);
-        //Channel3DProperties props = new Channel3DProperties();
-        //await VivoxService.Instance.JoinPositionalChannelAsync("ChannelName",ChatCapability.AudioOnly,props);
+        if (VivoxService.Instance == null)
+        {
+            await UnityServices.InitializeAsync();
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            await VivoxService.Instance.InitializeAsync();
+            VivoxService.Instance.LoggedIn += UserLoggedIn;
+            multiplayer.OnRoomJoined.AddListener(LogInToVivox);
+            multiplayer.OnRoomLeft.AddListener(LeftRoom);
+        }
     }
 
     async void LogInToVivox(Multiplayer arg0,Room arg1,User arg2)
     {
-        await VivoxService.Instance.LoginAsync();
+        if (!VivoxService.Instance.IsLoggedIn)
+        {
+            await VivoxService.Instance.LoginAsync();
+        }
     }
 
     void UserLoggedIn()
@@ -43,19 +44,14 @@ public class VivoxTest : MonoBehaviour
         Channel3DProperties props = new Channel3DProperties(64,10,1.0f, AudioFadeModel.InverseByDistance);
         await VivoxService.Instance.JoinPositionalChannelAsync(name,ChatCapability.AudioOnly,props);
         //await VivoxService.Instance.JoinGroupChannelAsync(name, ChatCapability.AudioOnly);
-        Debug.LogError("Joined Channel: " + name);
+        //await VivoxService.Instance.JoinEchoChannelAsync(name, ChatCapability.AudioOnly);
+        currentVoicechatLobby = name;
+        Debug.Log("Joined Channel: " + name);
     }
 
-    //async void LeftRoom(Multiplayer arg0)
-    //{
-    //    await VivoxService.Instance.LeaveChannelAsync(name);
-    //}
-
-    // Update is called once per frame
-    void Update()
+    async void LeftRoom(Multiplayer arg0)
     {
-        
-       
-
+        await VivoxService.Instance.LeaveChannelAsync(currentVoicechatLobby);
+        VivoxService.Instance.LogoutAsync();
     }
 }
