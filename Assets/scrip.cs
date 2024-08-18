bc356e3bde2882ba09785ae1704e36e15dc05ffe
tyronnr1 <tyronsantospereira@gmail.com>
2024-08-18 18:16:58 +0200

    RGFG

--- Assets/scrip.cs
+++ Assets/scrip.cs
@@ -20,13 +20,38 @@ public class TaggingBoxCollisionHandler : MonoBehaviour
         // Update logic, if any, goes here
     }
 
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject != null && !objectList.Contains(other.transform.root.gameObject) && other.transform.root.gameObject.name.Contains("PlayerNewPrefab"))
+    //    {
+    //        objectList.Add(other.transform.root.gameObject);
+    //    }
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject != null && !objectList.Contains(other.transform.root.gameObject) && other.transform.root.gameObject.name.Contains("PlayerNewPrefab"))
+        // Ensure the collider and its root are not null
+        if (other == null) return;
+
+        GameObject rootObject = other.transform.root.gameObject;
+
+        // Check if the collided object is a "PlayerNewPrefab" and not already in the list
+        if (rootObject.name.Contains("PlayerNewPrefab") && !objectList.Contains(rootObject))
         {
-            objectList.Add(other.transform.root.gameObject);
+            // Get the PreyComponent and HunterComponent from the children of the root object
+            Transform prey = rootObject.transform.Find("PreyComponent");
+            Transform hunter = rootObject.transform.Find("HunterComponent");
+
+            // If the PreyComponent exists and is active, and the HunterComponent is either non-existent or inactive
+            if (prey != null && prey.gameObject.activeSelf &&
+                (hunter == null || !hunter.gameObject.activeSelf))
+            {
+                // Add the root object to the objectList
+                objectList.Add(rootObject);
+            }
         }
     }
+
 
 
     public List<GameObject> GiveList()
