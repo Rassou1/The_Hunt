05c9f59b86af3f7d9c54f2c26f2211ffa2761e7a
Ibrahim <ibrra187@gmail.com>
2024-08-28 16:31:22 +0200

    Camera position updates to cameraholder every frame.

--- Assets/Scripts/PlayerStateMachine/CamToPlayerConnect.cs
+++ Assets/Scripts/PlayerStateMachine/CamToPlayerConnect.cs
@@ -4,13 +4,19 @@ public class CamToPlayerConnecter : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject cameraHolder;
+    P_StateManager stateManagerScript;
 
     void Start()
     {
         Transform cameraPos = playerPrefab.transform.Find("CameraPos");
         P_MoveCamera moveCameraScript = cameraHolder.GetComponent<P_MoveCamera>();
         moveCameraScript.cameraPosition = cameraPos;
-        P_StateManager stateManagerScript = playerPrefab.GetComponent<P_StateManager>();
+        stateManagerScript = playerPrefab.GetComponent<P_StateManager>();
+        stateManagerScript._cameraOrientation = cameraHolder.transform;
+    }
+
+    void Update()
+    {
         stateManagerScript._cameraOrientation = cameraHolder.transform;
     }
 }
